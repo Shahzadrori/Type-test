04d89ae3eb6d5a7cc9cbd3e55cde2fee9e45f53e
Shahzad <shahzadrori@gmail.com>
2021-02-03 13:03:20 +0500

    update

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const setofwords = [
     'My name is Shahzad and I live in Fasialabad.',
     'Hope that it is for you to understand Javascript',
     'If You wana get better then start understanding'
-]
+];
+const wordcounter = (str) =>{
+    let response =  str.split(" ").length;
+    return response;
+}
+
 
 var playit= () => {
     let random = Math.floor (Math.random()*setofwords.length);
@@ -16,7 +21,33 @@ var playit= () => {
     btn.innerHTML = 'Done'
 }
 
+var endit = ()=>{
+    let date = new Date();
+    endtime=date.getTime();
+    let totaltime = ((endtime - starttime)/1000);
+    let totstring = typewords.value;
+    let wordCount = wordcounter(totstring);
+    let speed = Math.round((wordCount / totaltime) * 60 );
+    // finalmsg += Comparewords(message.innerHTML,totstring);
+    let finalmsg = 'You typed at ' + speed + ' words per minute';
+    message.innerHTML = finalmsg;
+    finalmsg += Comparewords(message.innerHTML,totstring);
+}
+const Comparewords = (str1,str2)=>{
+    let word1 = str1.split(" ");
+    let word2 = str2.split(" ");
+    let cnt = 0;
 
+    word1.forEach(function (item ,index){
+        if(item == word2[index]){
+            cnt++;
+        }
+    })
+        let errorwords = (word1.length - cnt);
+        return (cnt + 'correct out of ' + word1.length + 'words and the total numberof error are ' + errorwords +'.') 
+    
+
+}
 btn.addEventListener('click', function () {
   if(this.innerText == 'Start'){
       typewords.disabled = false;
@@ -24,6 +55,6 @@ btn.addEventListener('click', function () {
   }else if(this.innerHTML = 'Done'){
       typewords.disabled = true;
       btn.innerHTML= 'Start';
-      endtime();
+      endit();
   }
 })
