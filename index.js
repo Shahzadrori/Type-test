2784097e467326cd920c4b866d8e1465a232dd6c
Shahzad <shahzadrori@gmail.com>
2021-02-03 12:11:03 +0500

    defining the functions

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ const message = document.getElementById('msg');
 const typewords = document.getElementById('mywords');
 const btn = document.getElementById('btn');
 let starttime, endtime;
-btn.addEventListener('click', () => {
+const setofwords = [
+    'My name is Shahzad and I live in Fasialabad.',
+    'Hope that it is for you to understand Javascript',
+    'If You wana get better then start understanding'
+]
+
+var playit= () => {
+    let random = Math.floor (Math.random()*setofwords.length);
+    message.innerHTML = setofwords[random];
+    let date = new Date();
+    starttime=date.getTime();
+    btn.innerHTML = 'Done'
+}
+
+
+btn.addEventListener('click', function () {
   if(this.innerText == 'Start'){
       typewords.disabled = false;
-      playit
+      playit();
+  }else if(this.innerHTML = 'Done'){
+      typewords.disabled = true;
+      btn.innerHTML= 'Start';
+      endtime();
   }
 })
